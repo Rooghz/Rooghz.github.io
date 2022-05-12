c910b6da41c2b1afffa1fa46105e37a506a05179
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2022-05-12 20:59:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,50 @@
-const form = document.querySelector('#searchform');
-const res = document.querySelector('#tableresult');
-var upd;
+const form = document.querySelector('#searchForm');
+const res = document.querySelector('#resTable');
+const cont = document.getElementById("allContaint");
 
 form.addEventListener('submit',(e)=>{
-
-   e.preventDefault();
-   if(upd){
-       clearTimeout(upd);
-   }
-
-   const ctype = form.elements.cointype.value;
-
-   fetchPrice(ctype);
+    e.preventDefault();
+    const ctype = form.elements.coinType.value;
+    cont.classList.add('mainClick');
+    cont.classList.remove('main');    
+    fetchPrice(ctype);
 
 });
 
-const fetchPrice= async(ctype) =>{
+const fetchPrice = async(ctype) =>{
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
+    showPrice(r.data.coin);
+}
 
-    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
-     const price = r.data.coin.price;
-     const volume = r.data.coin.volume;
-     const change = r.data.coin.priceChange1d;
-     const base =  r.data.coin.name;
-     const target = 'INR';
 
-    res.innerHTML =`<tr style="background-color:blue; color:white; font-weight:700">
+const showPrice = (coinData)=>{
+    const price = coinData.price;
+    const vol = coinData.volume;
+    const change = coinData.priceChange1d;
+    const coin = coinData.name;
+    const curr = 'USD';
+    var col= "green";
+    if(change<0){
+        col = "red";
+    }
+    res.innerHTML = `<tr class="bg-primary" style="color: white;">
     <td>
         Property
     </td>
     <td>
-         Value
+        Value
     </td>
 </tr>
 <tr>
-  <td>
-      ${base}
-  </td>
-  <td>${price} ${target}</td>
+    <td>${coin}</td>
+    <td style="color:${col};"><span style="font-size: 1.3em;">${price}</span> ${curr}</td>
 </tr>
 <tr>
-  <td>
-      Volume
-  </td>
-  <td>${volume}</td>
+    <td>Volume (24hrs)</td>
+    <td>${vol}</td>
 </tr>
 <tr>
-  <td>
-      Change
-  </td>
-  <td>${change}</td>
+    <td>Change (24hrs)</td>
+    <td style="color:${col};">${change} ${curr}</td>
 </tr>`;
-
-upd = setTimeout(()=>fetchPrice(ctype),10000);
-    
-}
+};
